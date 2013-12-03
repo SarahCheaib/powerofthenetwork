9c84bbd243b69f22ab6aa95309278074d5a275c8
DJ Mosca <djmosc@gmail.com>
2013-12-03 11:10:42 +0000

    Added in highlights

--- js/jquery.connected-home.js
+++ js/jquery.connected-home.js
@@ -7,200 +7,230 @@
     	var plugin = this;
     	this._defaults = defaults;
         this._name = pluginName;
-        
-    	this.init = function(){
-    		this.element = element;
+        this.element = $(element);
+        this.header = $('> .header', this.element);
 
+    	this.init = function(){
     		this.sections.init();
+            this.tooltips.init();
+            this.navigations.init();
     	};
 
     	this.sections = {
     		init: function(){
-    			var sections = plugin.sections;
-    			sections.section = $('.section', plugin.element);
-    			var id = sections.section.filter(':first-child').data('id');
+    			var sections = plugin.sections,
+                    tooltips = plugin.tooltips,
+                    navigations = plugin.navigations;
 
-    			sections.section.on('enter', function(e){
-    				var section = $(this);
+				sections.container = $('.sections', plugin.element);
+    			sections.section = $('.section', sections.container);
+    			var id = sections.section.filter(':first-child').data('id');
+				
+    			
+    			sections.currId = 0;
+    			sections.firstRun = true;
+				sections.btn = $('.section-btn', plugin.element);
+
+    			sections.section.on('enter.section', function(e){
+    				var section = $(this),
+    					id = section.data('id');
+
     				section.addClass('current');
     				section.addClass('enter');
-    				section.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', section.trigger('ready'));
-    			}).on('ready', function(){
-    				console.log('ready')
-    				// var section = $(this);
-    				// section.addClass('current');
+    				section.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+    					section.trigger('ready.section');
+    				});
+                    navigations.goto(id);
+                    plugin.element.addClass('section-'+id);
+    				sections.btn.removeClass('selected');
+    				sections.btn.filter('[data-id='+id+']').addClass('selected');
+                    
+    			}).on('ready.section', function(e){
+    				var section = $(this),
+                        id = section.data('id');
+    				section.removeClass('enter').addClass('ready');
+                    sections.currId = id;
+    			}).on('leave.section', function(e){
+    				var section = $(this);
+    				section.removeClass('ready').addClass('leave');
+    				section.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+    					section.trigger('exit.section');
+    				});
+
+    			}).on('exit.section', function(e){
+    				var section = $(this),
+                        id = section.data('id'),
+    					tooltip = tooltips.current();
+
+                    plugin.element.removeClass('section-'+id);
+    				section.removeClass('current leave zoom-in zoom-out');
+
+    				if(tooltip.hasClass('current')){
+    					tooltip.trigger('leave');
+    				}
+    			}).on('zoom-in.section', function(e){
+    				var section = $(this);
+	    			section.removeClass('zoom-out').addClass('zoom-in');
+    			}).on('zoom-out.section', function(e){
+    				var section = $(this);
+    				section.removeClass('zoom-in').addClass('zoom-out');
+    				section.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+    					$('.image', section).css({transformOrigin: '0 0'});
+    				});
     			});
-    			// sections.section.enter = function(){
-    			// 	console.log(this);
-    			// } 
-    			//console.log(sections.section);
-    			// var id = sections.elements.filter(':first-child').data('id');
-    			//sections.goto(id);
 
-    			// sections.elements.on('enter', function(){
-    			// 	console.log(this);
-    			// });
+    			sections.btn.on('click', function(){
+    				var id = $(this).data('id');
+    				sections.goto(id);
+    			});
+
 				sections.goto(id);
-    		},
-    		section: {
-    			enter: function(){
-	    			var sections = plugin.sections,
-	    			section = sections.elements.filter('[data-id=' + id + ']');
-	    			section.addClass('current');
-	    		},
-	    		ready: function(){
-
-	    		}
+				sections.firstRun = false;
     		},
     		goto: function(id){
-    			var sections = plugin.sections;
-    			section = sections.section.filter('[data-id=' + id + ']');
-    			section.trigger('enter');
+    			var sections = plugin.sections,
+    				currId = sections.currId;
+
+    			if(id != currId || sections.firstRun){
+	    			
+	    			newSection = sections.section.filter('[data-id=' + id + ']');
+					if(sections.firstRun){
+	    				newSection.trigger('enter');
+	    			} else {
+	    				currSection = sections.section.filter('[data-id=' + currId + ']');
+	    				currSection.trigger('leave.section');
+	    				currSection.on('exit.section', function(){
+	    					newSection.trigger('enter.section');
+	    				});
+	    			}
+	    		}
     		},
-    		
-    	}
+    		current: function(){
+    			return this.section.filter('[data-id='+this.currId+']');
+    		}	
+    	};
 
+    	this.tooltips = {
+    		init: function(){
+    			var tooltips = plugin.tooltips;
+     			tooltips.container = $('.tooltips', plugin.element);
+    			tooltips.currId = 0;
+    			tooltips.tooltip = $('.tooltip', tooltips.container);
+                tooltips.btn = $('.tooltip-btn', plugin.element);
 
-  //       this.element = element;
-  //       this.settings = $.extend( {}, defaults, options );
-        
+    		  	tooltips.tooltip.on('enter.tooltip', function(){
+    				var tooltip = $(this),
+                        id = tooltip.data('id'),
+    					sections = plugin.sections,
+    					section = sections.current(),
+    					position = {
+		    				top: tooltip.get(0).style.top,
+		    				left: tooltip.get(0).style.left,
+		    			};
 
+		    		$('.image', section).css({transformOrigin: position.left + ' '+ position.top});
+	   				tooltip.addClass('current enter');
+    				section.trigger('zoom-in.section');
+    				section.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+    					tooltip.trigger('ready.tooltip');
+    				});
 
-  //   	this.sections = {
-  //   		elements: $('.section')
-  //   	};
-  //   	this.sections.section = $('.section', this.element);
-  //   	this.images = {};
-  //   	this.images.image = $('.image', this.sections.section);
-  //   	this.navigation = {};
-  //   	this.navigation.btn = $('.section-btn', this.element);
-  //   	this.tooltips = {};
-  //   	this.tooltips.btn = $('.tooltip-btn', this.element);
-  // 		this.firstLoad = true;
-		// this.sections.currId = 0;
+                    tooltips.btn.removeClass('selected');
+                    tooltips.btn.filter('[data-id='+id+']').addClass('selected');
+    			}).on('ready.tooltip', function(){
+    				var tooltip = $(this);
+    				tooltip.addClass('ready').removeClass('enter');
+    			}).on('leave.tooltip', function(e, zoomOut){
+    				var tooltip = $(this),
+	    				sections = plugin.sections,
+    					section = sections.current();
+    				tooltip.addClass('leave').removeClass('ready').trigger('exit.tooltip');
 
-         this.init();
+    				section.trigger('zoom-out.section');
+	    		}).on('exit.tooltip', function(){
+					var tooltip = $(this);
+    				tooltip.removeClass('current leave');
+                    tooltips.btn.removeClass('selected');
+    			}).on('change.tooltip', function(){
+    				var tooltip = $(this),
+	    				currTooltip = tooltips.current();
+
+	    			currTooltip.one('exit.tooltip', function(){
+						tooltip.trigger('enter.tooltip');
+					}).addClass('leave').removeClass('ready').trigger('exit.tooltip');
+    			});
+
+    			$('.tooltip-btn, .close-btn', plugin.element).on('click', function(){
+    				var id = $(this).data('id');
+    				tooltips.goto(id);
+    			});
+    		},
+    		goto: function(id){
+    			var tooltips = plugin.tooltips,
+    				currId = tooltips.currId;
+    			
+    			if(id && id != currId){
+   					newTooltip = tooltips.tooltip.filter('[data-id='+id+']');
+    				
+	    			if(tooltips.tooltip.hasClass('current')){
+	    				newTooltip.trigger('change.tooltip');
+					} else {
+						newTooltip.trigger('enter.tooltip');
+					}
+
+					tooltips.currId = id;
+    			} else {
+    				tooltips.close();
+    			}
+    			
+			},
+			close: function(){
+				var tooltips = plugin.tooltips,
+					tooltip = tooltips.current();
+				tooltip.trigger('leave.tooltip');
+                tooltips.currId = 0;
+			},
+			current: function(){
+				return this.tooltip.filter('[data-id='+this.currId+']');
+			},
+			resize: function(){
+			
+			}
+    	};
+
+        this.navigations = {
+            init: function(){
+                var navigations = plugin.navigations;
+                navigations.container = $('.section-navigation-container', plugin.element);
+                navigations.navigation = $('.navigation', navigations.container);
+            },
+            goto: function(id){
+                var navigations = plugin.navigations,
+                    sections = plugin.sections,
+                    currId = sections.currId;
+                
+                console.log(id, currId);
+                if(id && id != currId){
+
+                    currNavigation = navigations.navigation.filter('[data-id='+currId+']');
+                    newNavigation = navigations.navigation.filter('[data-id='+id+']');
+                    console.log(newNavigation);
+                    if(newNavigation.length){
+                        newNavigation.addClass('current'); 
+                    }
+                    if(currNavigation.length){
+                        currNavigation.removeClass('current');
+                    }
+                } else {
+                   // navigations.navigation.removeClass('current');
+                }
+            }
+        };
+
+        this.init();
     }
 
-   //  Plugin.prototype = {
-   //      init: function() {
-
-   //      	$(window).on('load', this.loaded);
-        	
-			// var	id = this.sections.section.filter(':first-child').data('id'),
-			// 	plugin = this;
-
-			// this.navigation.btn.on('click', function(e) {
-			// 	e.preventDefault();
-			// 	var id = $(this).data('id');
-			// 	plugin.gotoSection(id);
-			// });
-
-			// plugin.gotoSection(id);
-
-			
-			// // this.images.image.each(function(){
-			// // 	var image = $(this);
-			// // 	if(image.parent().hasClass('image-mask')){
-			// // 		image.mask = image.parent();
-			// // 		image.move = {};
-			// // 		image.move.currX = 0;
-			// // 		image.move.mouseX = 0;
-			// // 		image.move.do = function(){
-			// // 			image.move.currX = parseFloat(image.css('left'));
-			// // 			var mousePercentageX = parseInt(((image.move.mouseX - image.mask.offset().left) / image.mask.outerWidth()) * 100);
-			// // 			var newPosX = parseFloat(-($('img', image).width() - image.mask.outerWidth()) * (mousePercentageX / 100));
-			// // 			newPosX = ((newPosX - image.move.currX) * 0.5) + image.move.currX;
-			// // 			image.css('left', newPosX+'px');
-			// // 		}
-
-			// // 		image.on('mouseenter', function(){
-			// // 			image.move.interval = setInterval(image.move.do, 33);
-			// // 		}).on('mouseleave', function(){
-			// // 			clearInterval(image.move.interval);
-			// // 		}).on('mousemove', function(e){
-			// // 			image.move.mouseX = e.pageX;
-			// // 		});
-			// // 	}
-			// // });
-
-
-			// // if (Modernizr.touch){
-			// // 	itemsNavigationMask.css({'overflow': 'scroll'})
-			// // } else {
-			// // 	itemsNavigation.hover(function(e){
-			// // 		navigationInterval = setInterval(moveNavigation, 33);
-			// // 	}, function(e){
-			// // 		clearInterval(navigationInterval);
-			// // 	});
-			
-			// // 	itemsNavigation.mousemove(function(e){
-			// // 		mouseYPos = e.pageY;
-			// // 	});
-			// // }
-
-			// // $('.map area', this.element).on('mouseenter', function(){
-			// // 	var area = $(this),
-			// // 		id = area.data('id');
-
-			// // 	$('.overlay', this.element).filter('[data-id='+id+']').fadeIn();
-			// // }).on('mouseleave', function(){
-			// // 	var area = $(this),
-			// // 		id = area.data('id');
-
-			// // 	$('.overlay', this.element).filter('[data-id='+id+']').fadeOut();
-			// // });
-
-			// // currSection.on('leave', function(){
-			// // 	if(currId != this.id){
-			// // 		animate out
-			// // 		oncomplete
-			// // 		this.trigger('exit');
-			// // 	}
-			// // });
-
-			// // currSection.on('exit', function(){
-			// // 	animated out
-			// // 	newSection.trigger('enter');
-			// // });
-
-			// // newSection.on('enter', function(){
-			// // 	animate in
-			// // 	oncomplete
-			// // 	this.trigger('ready');	
-			// // });
-
-			// // newSection.on('ready', function(){
-			// // 	animated in
-			// // });
-   //      },
-   //      loaded: function(){
-			// if($.fn.maphilight){
-	  //   		$('img[usemap]', this.element).maphilight();
-	  //       }
-   //      },
-
-   //      gotoSection: function (id) {
-   //      	if (id != this.sections.currId || this.firstLoad) {
-   //      		var section = this.sections.section.filter('[data-id=' + id + ']');
-        		
-   //      		// set current class to buttons
-			// 	this.navigation.btn.removeClass('selected');
-			// 	this.navigation.btn.filter('[data-id='+id+']').addClass('selected');
-			// 	this.sections.section.removeClass('current').hide();
-			// 	section.addClass('current').fadeIn();
-			// 	// this.sectsection.hide();
-			// 	// section.fadeIn().addClass('current');
-			// 	// this.firstLoad = false;
-	  //  			// this.sections.currId = id;
-   //      	}
-   //      },
-
-   //      gotoTooltip: function(id){
-   //      //	console.log();
-   //      }
-   //  };
-
+  
     $.fn[ pluginName ] = function ( options ) {
         return this.each(function() {
             if ( !$.data( this, "plugin_" + pluginName ) ) {
